65c391c8cad14040202c5dd1f878934bf4ad9afa
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-12-17 23:34:50 +0300

    Add table example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ButtonModule } from 'primeng/button';
+import { RatingModule } from 'primeng/rating';
+import { RippleModule } from 'primeng/ripple';
+import { TableModule } from 'primeng/table';
 import { AppComponent } from './app.component';
+import { TableRowExpansionDemo } from './tablerowexpansiondemo';
+import { ProductService } from "./service/productservice";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableRowExpansionDemo
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ButtonModule,
+    RatingModule,
+    RippleModule,
+    TableModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
