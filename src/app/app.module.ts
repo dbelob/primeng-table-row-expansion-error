cc0fdfff9a49280c7881218295d988a4a377c822
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2024-05-31 14:15:04 +0300

    Update Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { ButtonModule } from 'primeng/button';
 import { RatingModule } from 'primeng/rating';
@@ -10,22 +10,15 @@ import { AppComponent } from './app.component';
 import { TableRowExpansionDemo } from './tablerowexpansiondemo';
 import { ProductService } from "./service/productservice";
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    TableRowExpansionDemo
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    ButtonModule,
-    RatingModule,
-    RippleModule,
-    TableModule
-  ],
-  providers: [ProductService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        TableRowExpansionDemo
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        ButtonModule,
+        RatingModule,
+        RippleModule,
+        TableModule], providers: [ProductService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {
 }
