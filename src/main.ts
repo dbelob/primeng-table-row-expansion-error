16dbc5c278361e99eb95b29699ed7ceb2666f0c2
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2025-12-14 23:55:21 +0300

    Update Angular

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import 'zone.js';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from '@angular/platform-browser';
 
 import { AppModule } from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowser().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => console.error(err));
