ed272247fbf23f2aab7bb9f0419850f752303a7d
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2026-07-24 13:53:10 +0300

    Fix compilation

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,18 @@
-import { provideZoneChangeDetection, importProvidersFrom } from "@angular/core";
-import 'zone.js';
-import { platformBrowser, BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-
+import { provideZoneChangeDetection } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient, withXhr, withInterceptorsFromDi } from "@angular/common/http";
+import { definePreset } from '@primeuix/themes';
+import Aura from '@primeuix/themes/aura';
 
 import { ProductService } from "./app/service/productservice";
-import { provideHttpClient, withXhr, withInterceptorsFromDi } from "@angular/common/http";
 import { providePrimeNG } from "primeng/config";
-import { definePreset } from "@primeuix/themes";
-import { ChevronDown } from "@primeicons/angular/chevron-down";
-import { ChevronRight } from "@primeicons/angular/chevron-right";
-import { FormsModule } from "@angular/forms";
-import { ButtonModule } from "primeng/button";
-import { RatingModule } from "primeng/rating";
-import { RippleModule } from "primeng/ripple";
-import { TableModule } from "primeng/table";
-import { Tag } from "primeng/tag";
 import { AppComponent } from "./app/app.component";
-
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(BrowserModule, ChevronDown, ChevronRight, FormsModule, ButtonModule, RatingModule, RippleModule, TableModule, Tag),
-        ProductService,
         provideHttpClient(withXhr(), withInterceptorsFromDi()),
+        provideZoneChangeDetection(),
+        ProductService,
         providePrimeNG({
             theme: {
                 preset: definePreset(Aura, {
@@ -42,7 +32,7 @@ bootstrapApplication(AppComponent, {
                         }
                     }
                 })
-            },
+            }
         })
     ]
 })
