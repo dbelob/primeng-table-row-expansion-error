0edc0f99be72f173ae28f3546d931999b369413c
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2026-07-24 13:36:36 +0300

    Convert all components, directives and pipes to standalone

--- src/app/tablerowexpansiondemo.ts
+++ src/app/tablerowexpansiondemo.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Product } from './domain/product';
 import { ProductService } from './service/productservice';
+import { Bind } from 'primeng/bind';
+import { Table, SortableColumn, SortIcon, RowToggler } from 'primeng/table';
+import { ButtonDirective, Button } from 'primeng/button';
+import { Ripple } from 'primeng/ripple';
+import { ChevronDown } from '@primeicons/angular/chevron-down';
+import { ChevronRight } from '@primeicons/angular/chevron-right';
+import { Rating } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
+import { Tag } from 'primeng/tag';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
     selector: 'app-table-row-expansion-demo',
     templateUrl: './tablerowexpansiondemo.html',
     changeDetection: ChangeDetectionStrategy.Eager,
-    standalone: false
+    imports: [Bind, Table, SortableColumn, SortIcon, ButtonDirective, Ripple, RowToggler, ChevronDown, ChevronRight, Rating, FormsModule, Tag, Button, CurrencyPipe]
 })
 export class TableRowExpansionDemo implements OnInit {
   products: Product[] = [];
